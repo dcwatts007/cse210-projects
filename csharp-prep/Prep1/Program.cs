f0991666ae99f0f7c09aacdadcba9d3ec4dcae5d
Daniel Watts <dcwatts007>
2023-09-16 20:37:02 -0600

    made it add their last name at the end.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,6 +8,6 @@ class Program
         String first = Console.ReadLine();
         Console.WriteLine("What is your last name?");
         String last = Console.ReadLine();
-        Console.WriteLine($"Your name is {last}, {first}");
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
