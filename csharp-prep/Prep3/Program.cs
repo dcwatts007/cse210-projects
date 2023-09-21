7f537ea6224f410a344589d2490cc451c4eb389b
Daniel Watts <dcwatts007>
2023-09-20 19:10:47 -0600

    added an option to continue playing

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,30 +1,37 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int magic;//the number to be guessed
-        int number;//the guess of the person
-        int counter = 0;//the amount of guesses used
-        //generates a random number for the user to guess
-        Random ran = new Random();
-        magic = ran.Next(1,101);
-        //loops until the user guesses the magic number. 
-        do
+        String answer="yes";
+        while (answer.ToLower()=="yes")
         {
-            Console.WriteLine("What is your guess?");
-            number = int.Parse(Console.ReadLine());
-            //checks if the guess is lower or higher than the magic number
-            if(number>magic)
+            int magic;//the number to be guessed
+             int number;//the guess of the person
+             int counter = 0;//the amount of guesses used
+             //generates a random number for the user to guess
+            Random ran = new Random();
+            magic = ran.Next(1,101);
+            //loops until the user guesses the magic number. 
+            do
             {
-                Console.WriteLine("Lower");
-            }else if(number<magic)
-            {
-                Console.WriteLine("Higher");
-            }
-            counter ++;
-        } while(number!=magic);
-        Console.WriteLine($"You guessed it in {counter} guesses!");
+              Console.WriteLine("What is your guess?");
+              number = int.Parse(Console.ReadLine());
+              //checks if the guess is lower or higher than the magic number
+              if(number>magic)
+              {
+                 Console.WriteLine("Lower");
+             }else if(number<magic)
+             {
+                   Console.WriteLine("Higher");
+              }
+               counter ++;
+            } while(number!=magic);
+             Console.WriteLine($"You guessed it in {counter} guesses!");
+             Console.WriteLine("Play again?");
+             answer=Console.ReadLine();
+        }
     }
 }
