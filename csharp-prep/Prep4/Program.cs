82089d8efe9e3514be0efc9d1cfb169f4763435c
Daniel Watts <dcwatts007>
2023-09-20 19:50:16 -0600

    met the stretch goals

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -9,6 +9,7 @@ class Program
         int sum = 0;
         double average = 0;
         int big = 0;
+        int small = 0;
         Console.WriteLine("Enter a list of numbers, type  0 when finished\nEnter number:");
         input=int.Parse(Console.ReadLine());
         while(input!=0)
@@ -17,6 +18,7 @@ class Program
             Console.WriteLine("Enter number:");
             input=int.Parse(Console.ReadLine());
         }
+        numbers.Sort();
         foreach(int i in numbers)
         {
             sum+=i;
@@ -24,8 +26,16 @@ class Program
             {
                 big=i;
             }
+            if(i>0&&(small==0||i<small))
+            {
+                    small=i;
+            }
         }
         average=(sum*1.0)/numbers.Count();
-        Console.WriteLine($"The sum is: {sum}\nThe average is: {average}\nThe largest number is: {big}");
+        Console.WriteLine($"The sum is: {sum}\nThe average is: {average}\nThe largest number is: {big}\nThe smallest positive number is: {small}\n The sorted list is: ");
+        foreach(int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
