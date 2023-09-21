993a959b06b8641ed31094acf77a6181032c5fd5
Daniel Watts <dcwatts007>
2023-09-20 20:02:42 -0600

    did the assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int i)
+    {
+        return i*i;
+    }
+    static void DisplayResult(string name,int i)
+    {
+        Console.WriteLine($"{name}, the square of your number is {i}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(),SquareNumber(PromptUserNumber()));
     }
 }
