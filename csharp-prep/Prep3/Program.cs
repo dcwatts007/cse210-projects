89088b773074c3d05abad55dd6e5e765da91c4a6
Daniel Watts <dcwatts007>
2023-09-20 18:51:39 -0600

    Did requirements 1 and 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magic;
+        int number;
+        Console.WriteLine("Whit is the magic number?");
+        magic = int.Parse(Console.ReadLine());
+        //loops until the user guesses the magic number. 
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            number = int.Parse(Console.ReadLine());
+            //checks if the guess is lower or higher than the magic number
+            if(number>magic)
+            {
+                Console.WriteLine("Lower");
+            }else if(number<magic)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while(number!=magic);
+        Console.WriteLine("You guessed it!");
     }
 }
