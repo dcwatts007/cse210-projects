415bc9afa51b49ce4b43ab7a0bbeb63400a71118
Daniel Watts <dcwatts007>
2023-09-28 13:59:25 -0600

    did the luc test on sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Value for x: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Value for y: ");
+        int y = int.Parse(Console.ReadLine());
+        for(int i=0;i<y;i++)
+        {
+            x=48271*x;
+            x%=Convert.ToInt32((Math.Pow(2,31)-1));
+        }
+        Console.WriteLine($"Result: {x}");
     }
 }
